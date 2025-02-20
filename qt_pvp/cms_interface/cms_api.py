9e2a89fff7fac1003695e09ae0e28b31a8623f19
PunchyArchy <ksmdrmvstchny@gmail.com>
2025-02-20 12:12:57 +0500

    Более информативное логирование процесса скачивания видео

--- qt_pvp/cms_interface/cms_api.py
+++ qt_pvp/cms_interface/cms_api.py
@@ -118,10 +118,11 @@ def wait_and_get_dwn_url(jsession, download_task_url):
         response_json = response.json()
         result = response_json["result"]
         if result == 11 and response_json["oldTaskAll"]["dph"]:
+            logger.debug(f"Download done!")
             logger.debug(f'Get path: {str(response_json["oldTaskAll"]["dph"])}')
             return response_json["oldTaskAll"]["dph"]
         else:
-            logger.debug(f"Could not get download path. {response_json}")
+            logger.debug(f"Downloading...")
             time.sleep(2)
     # while result != 11:
     #    if not return_path:
