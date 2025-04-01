8b069f07fa167dcbfeace9f8dbaaa066d010fabe
PunchyArchy <ksmdrmvstchny@gmail.com>
2025-04-01 10:50:37 +0500

    Добавил проверку не законченных выгрузок

--- main_operator.py
+++ main_operator.py
@@ -93,10 +93,10 @@ class Main:
                 datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S") -
                 datetime.datetime.strptime(start_time,
                                            "%Y-%m-%d %H:%M:%S")).total_seconds()
-        if time_difference > 900:
+        if time_difference > 600:
             end_time = (datetime.datetime.strptime(start_time,
                                                    "%Y-%m-%d %H:%M:%S") +
-                        datetime.timedelta(seconds=900)).strftime(
+                        datetime.timedelta(seconds=600)).strftime(
                 "%Y-%m-%d %H:%M:%S")
         else:
             logger.debug(f"f{reg_id}. Time difference is too short "
